56db32f3cf681d138a85b8e63f733af655c505f6
jddbfhkiykg <43162644+jddbfhkiykg@users.noreply.github.com>
2018-10-12 16:25:23 +0530

    Add files via upload

---
+++ ttt.py
@@ -0,0 +1,42 @@
+a=['1','2','3','4','5','6','7','8','9']
+def print_Board():
+    print(a[0],'|',a[1],'|',a[2])
+    print("........")
+    print(a[3],'|',a[4],'|',a[5])
+    print("........")
+    print(a[6],'|',a[7],'|',a[8])
+PlayerOneTurn = True
+while True:
+        print_Board()
+        p=input("choose an available place")
+        if(p in a):
+            if(a[int(p)-1]=='x' or a[int(p)-1]=='o'):
+                print("place taken, choose another place")
+                continue
+            else:
+                    if PlayerOneTurn:
+                            print("player 1>>")
+                            a[int(p)-1]= 'x'
+                            PlayerOneTurn = not PlayerOneTurn
+                    else:
+                            print("player 2 >>")
+                            a[int(p)-1]= 'o'
+                            PlayerOneTurn = not PlayerOneTurn
+                    for i in (0,3,6):
+                            if(a[i]==a[i+1] and a[i]==a[i+2]):
+                                    print("Game over")
+                                    exit()
+                    for i in range (3):
+                            if(a[i]==a[i+3] and a[i]==a[i+6]):
+                                    print("Game over")
+                                    exit()
+                    if(a[0]==a[4] and a[0]==a[8]):
+                                    print("game over")   
+                                    exit()
+                    if(a[2]==a[4] and a[2]==a[6]):
+                                    print("game over")
+                                    exit()
+        else:
+                 print("you have entered an invalied position")
+                 continue
+                       
